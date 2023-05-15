997619e959849f8b306bfc231e50cc03a96c87d1
isnendyan <isnendyankp@gmail.com>
2023-05-15 13:11:42 +0700

    learn module fs with require() & create file with writeFilesync

--- first-app.js
+++ first-app.js
@@ -1,2 +1,10 @@
-// running node js = node first-app.js(file.js)
-console.log('Hello from Node.js');
+// running node js = node first-app.js(node file.js)
+// console.log('Hello from Node.js');
+
+//Untuk bisa menggunakan modul fs kita harus mengimpornya telebih dahulu dengan fungsi require().
+const fs= require('fs');
+
+
+//lalu membuat file txt dengan menggunakan writeFileSync
+fs.writeFileSync('hello.txt', 'Hello from Node.js');
+
