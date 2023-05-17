1816a07411133b0123481138bc18eba7d562c635
isnendyan <isnendyankp@gmail.com>
2023-05-17 14:38:25 +0700

    Refresher code: let & const

--- play.js
+++ play.js
@@ -1,6 +1,8 @@
 var name = 'Nendy';
-var age = '31';
+let age = '31';
 var hasHobbies = 'play MMORPG';
+
+age = 32;
 
 function summarizeUser(userName, userAge, userHasHobby) {
     return (
@@ -17,3 +19,4 @@ console.log(summarizeUser(name, age, hasHobbies));
 
 // if use node play.js on command the result will be:
 // Name is Nendy, age is 31 and the user has hobbies: play MMORPG
+// update age into 32: the print will be : Name is Nendy, age is 32 and the user has hobbies: play MMORPG
