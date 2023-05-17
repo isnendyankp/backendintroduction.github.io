ac0e506eceb5988c7cb3818945e414eee01957d2
isnendyan <isnendyankp@gmail.com>
2023-05-17 14:44:10 +0700

    Refresher code: understanding arrow functions

--- play.js
+++ play.js
@@ -4,19 +4,31 @@ var hasHobbies = 'play MMORPG';
 
 age = 32;
 
-function summarizeUser(userName, userAge, userHasHobby) {
-    return (
-        'Name is ' +
-        userName +
-        ', age is ' +
-        userAge +
-        ' and the user has hobbies: ' +
-        userHasHobby
-    );
-}
+const summarizeUser = (userName, userAge, userHasHobby) => {
+  return (
+    'Name is ' +
+    userName +
+    ', age is ' +
+    userAge +
+    ' and the user has hobbies: ' +
+    userHasHobby
+  );
+};
+
+// Understanding arrow function
+// const add = (a, b) => a + b;
+// const addOne = a => a + 1;
+const addRandom = () => 1 + 2;
+
+// console.log(add(1, 2));
+// console.log(addOne(1));
+console.log(addRandom());
 
 console.log(summarizeUser(name, age, hasHobbies));
 
 // if use node play.js on command the result will be:
 // Name is Nendy, age is 31 and the user has hobbies: play MMORPG
 // update age into 32: the print will be : Name is Nendy, age is 32 and the user has hobbies: play MMORPG
+// arrow functions Print: 
+// 3
+// Name is Nendy, age is 32 and the user has hobbies: play MMORPG
