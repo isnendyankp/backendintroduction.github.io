c81bc2607080477ff42420e1ca78791b558b9981
isnendyan <isnendyankp@gmail.com>
2023-05-19 14:27:22 +0700

    Node.js: learn ... method for pull out all ele/proper

---
+++ spreadrestoperators.js
@@ -0,0 +1,19 @@
+const person = {
+  name: 'Max',
+  age: 29,
+  greet() {
+    console.log('Hi, I am ' + this.name);
+  }
+};
+
+const hobbies = ['Sports', 'Cooking'];
+// for (let hobby of hobbies) {
+//     console.log(hobby);
+// }
+// console.log(hobbies.map(hobby => 'Hobby: ' + hobby));
+// console.log(hobbies);
+
+// symbol ... for Take the array or object after the operator and pull put all the elements or properties
+const copiedArray = [...hobbies];
+console.log(copiedArray);
+// Result : [ 'Sports', 'Cooking' ]
